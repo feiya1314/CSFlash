d8a7b8c21092a21d0142017f714f76beaed83398
feiya1314 <yufei1313@gmail.com>
2019-01-26 23:50:36 +0800

    add cursor

--- cassandraTest/src/main/java/com/yufeiblog/cassandra/common/Cursor.java
+++ cassandraTest/src/main/java/com/yufeiblog/cassandra/common/Cursor.java
@@ -1,7 +1,12 @@
 package com.yufeiblog.cassandra.common;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yufeiblog.cassandra.exception.CSException;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 abstract class Cursor implements Serializable {
@@ -12,7 +17,7 @@ abstract class Cursor implements Serializable {
     public static Cursor convertToCursor(String cursor) {
         char prefix = cursor.charAt(0);
         Cursor newCursor = null;
-        String realCursor = String.copyValueOf(cursor.toCharArray(),1,cursor.length()-1);
+
         switch (prefix) {
             case '0':
                 newCursor = new PagingStateCursor();
@@ -22,6 +27,13 @@ abstract class Cursor implements Serializable {
                 break;
             default:
                 throw new CSException("invalid cursor");
+        }
+        String realCursor = String.copyValueOf(cursor.toCharArray(), 1, cursor.length() - 1);
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            newCursor = objectMapper.readValue(realCursor, Cursor.class);
+        } catch (IOException  e){
+            throw new CSException("invalid cursor,convert to cursor failed");
         }
 
         return newCursor;
