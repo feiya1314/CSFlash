f6ea4e2c2435cdfd35835670edad24daf2ec27f2
feiya1314 <yufei1313@gmail.com>
2019-01-28 22:37:44 +0800

    add find

--- cassandraTest/src/main/java/com/yufeiblog/cassandra/service/BaseService.java
+++ cassandraTest/src/main/java/com/yufeiblog/cassandra/service/BaseService.java
@@ -19,7 +19,7 @@ public class BaseService {
     protected String grantPermissionModify = "GRANT MODIFY ON ALL KEYSPACES TO '%s'";
 
     protected SessionRepository sessionRepository;
-    protected Map<String, TableDefination> tableDefinationMap = new ConcurrentHashMap<>();
+
 
     public BaseService(SessionRepository sessionRepository) {
         this.sessionRepository = sessionRepository;
@@ -37,49 +37,9 @@ public class BaseService {
         return tableMetadata != null;
     }
 
-    protected TableDefination getTableDefination(int appId, String tableName) {
-        TableDefination tableDefination = tableDefinationMap.get(buildTableDefinationKey(appId,tableName));
-        if (tableDefination == null) {
-            tableDefination = new TableDefination();
-            TableMetadata tableMetadata = sessionRepository.getCluster().getMetadata().getKeyspace(Utils.getKeyspace(appId)).getTable(tableName);
-            return getTableDefinationFromMeta(tableMetadata);
-        }
 
-        return tableDefination;
-    }
 
-    private String buildTableDefinationKey(int appId, String tableName) {
-        return appId + "_" + tableName;
-    }
 
-    private TableDefination getTableDefinationFromMeta(TableMetadata tableMetadata){
-        TableDefination tableDefination = new TableDefination();
-        List<ColumnMetadata> pks = tableMetadata.getPartitionKey();
-        List<ColumnMetadata> primaryKeys = tableMetadata.getPrimaryKey();
-        int size = pks.size()+primaryKeys.size();
-        String[] primary = new String[size];
-        List<ColumnMetadata> allPrimaryKeys = new ArrayList<>();
-        allPrimaryKeys.addAll(pks);
-        allPrimaryKeys.addAll(primaryKeys);
-        int i =0 ;
-        for (ColumnMetadata columnMetadata : allPrimaryKeys){
-            primary[i++] = columnMetadata.getName();
-        }
-        tableDefination.setPrimaryKeys(primary);
-        tableDefination.setPartitionKeyCount(pks.size());
 
-        List<ColumnMetadata> columns = tableMetadata.getColumns();
-        Column[] targetColumns = new Column[columns.size()];
-        i=0;
-        for (ColumnMetadata columnMetadata:columns){
-            targetColumns[i] = new Column();
-            targetColumns[i].setColumnName(columnMetadata.getName());
-            DataType type = columnMetadata.getType();
-            targetColumns[i].setType(type.getName().name());
-            i++;
-        }
-        tableDefination.setColumns(targetColumns);
-        List<ClusteringOrder> clusteringOrders =  tableMetadata.getClusteringOrder();
-        return tableDefination;
-    }
+
 }
