ce601bc2600388e3e4840991a3ebd11890006d32
feiya1314 <yufei1313@gmail.com>
2019-06-02 18:52:47 +0800

    add repair test

---
+++ cassandraTest/src/test/java/com/yufeiblog/cassandra/test/RepairTest.java
@@ -0,0 +1,10 @@
+package com.yufeiblog.cassandra.test;
+
+public class RepairTest extends BaseTest{
+    @Override
+    protected void test() {
+
+    }
+
+    //system.size_estimates 表中 里面记录了每个表在每个token段上大约有多少分区key（partitions_count）以及每个分区key的大小（mean_partition_size）
+}
