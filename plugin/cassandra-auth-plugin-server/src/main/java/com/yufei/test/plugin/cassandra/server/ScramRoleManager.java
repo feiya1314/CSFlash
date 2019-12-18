f29a82600c33a9fc613c594411266dda51c987f7
feiya1314 <yufei1313@gmail.com>
2019-12-19 00:29:32 +0800

    add test

--- plugin/cassandra-auth-plugin-server/src/main/java/com/yufei/test/plugin/cassandra/server/ScramRoleManager.java
+++ plugin/cassandra-auth-plugin-server/src/main/java/com/yufei/test/plugin/cassandra/server/ScramRoleManager.java
@@ -4,8 +4,10 @@ import org.apache.cassandra.auth.AuthenticatedUser;
 import org.apache.cassandra.auth.CassandraRoleManager;
 import org.apache.cassandra.auth.RoleOptions;
 import org.apache.cassandra.auth.RoleResource;
+import org.apache.cassandra.cql3.QueryProcessor;
 import org.apache.cassandra.exceptions.RequestExecutionException;
 import org.apache.cassandra.exceptions.RequestValidationException;
+import org.apache.cassandra.service.QueryState;
 
 public class ScramRoleManager extends CassandraRoleManager {
     public ScramRoleManager(String username) {
@@ -14,11 +16,13 @@ public class ScramRoleManager extends CassandraRoleManager {
     @Override
     public void createRole(AuthenticatedUser performer, RoleResource role, RoleOptions options) throws RequestValidationException, RequestExecutionException {
         super.createRole(performer, role, options);
+        QueryProcessor.execute("",consistencyForRole(role.getRoleName()),QueryState.forInternalCalls());
     }
 
     public ScramRoleManager() {
         super();
         //consistencyForRole()
+       // QueryProcessor.execute("",QueryState.forInternalCalls())
     }
 
     @Override
